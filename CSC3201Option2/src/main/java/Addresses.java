f4f7e461e2f572d7b433fa789a50d50bf57d1d17
currickulus <thomas.dewing@gmail.com>
2024-08-18 18:07:18 -0700

    Add files via upload

---
+++ CSC3201Option2/src/main/java/Addresses.java
@@ -0,0 +1,11 @@
+
+public class Addresses {
+	
+	
+	public static void main(String[] args) {
+	System.out.println("Tacos y Hot Dogs Sonora");
+	System.out.println("2560 Las Vegas Blvd N,");
+	System.out.println("Las Vegas, NV, 89030");
+
+}
+}
